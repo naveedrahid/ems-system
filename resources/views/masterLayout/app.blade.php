c5a43a3f246828e25ad36cbac825846135a22b4c
syedahsanapprato <syed@apprato.com.au>
2024-05-08 17:21:30 +0500

    copy right changes

--- resources/views/masterLayout/app.blade.php
+++ resources/views/masterLayout/app.blade.php
@@ -298,7 +298,7 @@
             <div class="pull-right hidden-xs">
                 <b>Version</b> 2.3.6
             </div>
-            <strong>Copyright &copy; 2018-2019 <a href="https://www.afaicon.com">AFA Icon</a>.</strong> All rights
+            <strong>Copyright &copy; 2018-2019 <a href="#">Pixel 360</a>.</strong> All rights
             reserved.
         </footer>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" crossorigin="anonymous"
