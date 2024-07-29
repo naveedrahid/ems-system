d31f2fa7e497daa301fe681259379d4937032baa
syedahsanapprato <syed@apprato.com.au>
2024-07-29 08:49:52 +0500

    menu changes

--- resources/views/masterLayout/app.blade.php
+++ resources/views/masterLayout/app.blade.php
@@ -354,12 +354,12 @@
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview">
-                                    <li class="nav-item">
+                                    {{-- <li class="nav-item">
                                         <a href="{{ route('department.create') }}" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
                                             <p>Add Department</p>
                                         </a>
-                                    </li>
+                                    </li> --}}
                                     <li class="nav-item">
                                         <a href="{{ route('department.index') }}" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
